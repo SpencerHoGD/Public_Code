e5637b750ae5eba929be652747e0e74032b017d3
LawyZheng <lawyzheng1106@gmail.com>
2019-08-31 21:36:48 +0900

    移植了数据库

--- create_wordloud.py
+++ create_wordloud.py
@@ -1,38 +1,34 @@
 '''
 通过sqlite引擎，读取数据库数据
 '''
-
+import jieba
 import pandas
 import wordcloud
 import json
 from sqlalchemy import create_engine
 
-path = '/Users/lawyzheng/Desktop/greedyai_learning/'
+path = '/Users/lawyzheng/Desktop/Code/'
 
-#连接数据库
-engine = create_engine('sqlite:///' + path + 'toutiao_hot.db')
+# 连接数据库
+engine = create_engine('sqlite:///' + path + 'spider.db')
 df = pandas.read_sql('tb_toutiao_hot', con=engine, index_col='index')
-
-#将json数据转化成Python数据
-for col in df.columns:
-	if df[col].dtype == 'object':
-		df[col] = list(map(json.loads, df[col]))
 
 # 获取文章abstract数据
 abstract_list = df.abstract.to_list()
-for index, each in enumerate(abstract_list):
-	if not each:
-		print(index)
-abstract = "\n".join(abstract_list)
+abstract_list = list(map(jieba.cut, abstract_list))
+abstract_list = list(map(list, abstract_list))
+abstract = " ".join([' '.join(l) for l in abstract_list])
+# abstract = "\n".join(abstract_list)
 
 # 获取文章的tags数据
 tags_list = df.article_tags.to_list()
-tags = '\n'.join([' '.join(l) for l in tags_list])
+tags = ' '.join(tags_list)
 
-#清洗数据
-stopwords = {'人生第一份工作', '胜利退出演艺圈', '我的第一部5G手机', '澎湃新闻', '4月吃什么', 
-             '广州恒大淘宝足球俱乐部', '跳槽那些事儿', '越投入越精彩', '不完美妈妈', '原汁原味的德系SUV', '新闻',
-             '头号大赢家', '理财大赛第二季','我在宫里做厨师'}
+# 清洗数据
+with open('/Users/lawyzheng/Desktop/Code/Public_Code/stopwords.txt', 'r') as f:
+    stopwords = f.read()
+
+stopwords = set(stopwords.split('\n'))
 wc = wordcloud.WordCloud(width=800, height=600, font_path='msyh.ttf', stopwords=stopwords)
 
 wc.generate(abstract + tags)
--- weibo_resou.py
+++ weibo_resou.py
@@ -1,10 +1,10 @@
 import requests
 import time
-import datetime
 import pandas
 import random
 import logging
 from sqlalchemy import create_engine
+
 
 def set_logger():
     logger = logging.getLogger(__name__)
@@ -13,11 +13,11 @@ def set_logger():
     #文件输出
     log_path = '/Users/lawyzheng/Library/Logs/weibo_resou.log'
     fh = logging.FileHandler(log_path)
-    fh.setLevel(logging.WARNING)
+    fh.setLevel(logging.INFO)
 
     #Stream输出
     sh = logging.StreamHandler()
-    sh.setLevel(logging.INFO)
+    sh.setLevel(logging.WARNING)
 
     #设置format
     fmt = '%(asctime)s - %(levelname)s - %(message)s'
@@ -51,12 +51,12 @@ def get_json_data(timestamp):
     url = 'https://m.weibo.cn/api/container/getIndex'
 
     headers = {
-                'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-                'Accept' : 'application/json, text/plain, */*',
-                'MWeibo-Pwa' : '1',
-                'Sec-Fetch-Mode' : 'cors',
-                'X-Requested-With' : 'XMLHttpRequest',
-                'Referer' : 'https://m.weibo.cn/p/index?'\
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+                'Accept': 'application/json, text/plain, */*',
+                'MWeibo-Pwa': '1',
+                'Sec-Fetch-Mode': 'cors',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Referer': 'https://m.weibo.cn/p/index?'\
                         'containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&'\
                         'title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C&'\
                         'extparam=pos%3D0_0%26mi_cid%3D100103%26cate%3D10103%26filter_type%3Drealtimehot%26c_type%3D30%26display_time%3D{}&'\
@@ -64,11 +64,11 @@ def get_json_data(timestamp):
     }
 
     params = {
-            'containerid' : '106003type=25&t=3&disable_hot=1&filter_type=realtimehot',
-            'title' : '微博热搜',
-            'extparam' : 'pos=0_0&mi_cid=100103&cate=10103&filter_type=realtimehot&c_type=30&display_time=%d' % timestamp,
-            'luicode' : '10000011',
-            'lfid' : '231583'
+            'containerid': '106003type=25&t=3&disable_hot=1&filter_type=realtimehot',
+            'title': '微博热搜',
+            'extparam': 'pos=0_0&mi_cid=100103&cate=10103&filter_type=realtimehot&c_type=30&display_time=%d' % timestamp,
+            'luicode': '10000011',
+            'lfid': '231583'
     }
 
     resp = requests.get(url, headers=headers, params=params)
@@ -79,12 +79,12 @@ def get_json_data(timestamp):
 
 def append_new_item_to_dataframe(target, timestamp, dataframe):
     data = {
-        "title" : target['desc'],
-        "hot_index" : int(target['desc_extr']),
-        "start_time" : timestamp,
-        "end_time" : timestamp,
-        "lasting_time" : 0,
-        "still_in" : True
+        "title": target['desc'],
+        "hot_index": int(target['desc_extr']),
+        "start_time": timestamp,
+        "end_time": timestamp,
+        "lasting_time": 0,
+        "still_in": True
     }
 
     item = pandas.DataFrame(data, index=[0])
@@ -94,83 +94,65 @@ def append_new_item_to_dataframe(target, timestamp, dataframe):
 
 
 def update_item_in_dataframe(item, target ,dataframe, timestamp):
-    #获取index
+    # 获取index
     index = item.index[0]
-    #更新hot_index
+    # 更新hot_index
     dataframe.at[index, 'hot_index'] = int(target['desc_extr']) if dataframe.at[index, 'hot_index'] < int(target['desc_extr']) else dataframe.at[index, 'hot_index']
-    #更新end_time
+    # 更新end_time
     dataframe.at[index, 'end_time'] = timestamp
-    #更新在是否还在热搜
+    # 更新在是否还在热搜
     dataframe.at[index, 'still_in'] = True
-    #更新在热搜时间
+    # 更新在热搜时间
     dataframe.at[index, 'lasting_time'] = timestamp - dataframe.at[index, 'start_time']
 
 
 def format_data(dataframe, targets, timestamp):
-    for target  in targets:
-        #不属于热搜榜
+    for target in targets:
+        # 不属于热搜榜
         if not target.get('desc_extr'):
             continue
 
-        #如果dataframe为空，直接添加数据
+        # 如果dataframe为空，直接添加数据
         if dataframe.empty:
             dataframe = append_new_item_to_dataframe(target, timestamp, dataframe)
 
-
         else:
             item = dataframe[dataframe.title == target['desc']]
-            #如果不在dataframe中，则添加该数据
+            # 如果不在dataframe中，则添加该数据
             if item.empty:
                 dataframe = append_new_item_to_dataframe(target, timestamp, dataframe)
 
-            #如果在dataframe中
+            # 如果在dataframe中
             else:
                 update_item_in_dataframe(item, target, dataframe, timestamp)
-
 
     return dataframe
 
 
 def main(logger):
-    #创建数据库连接，读取数据库
-    engine = create_engine('sqlite:////Users/lawyzheng/Desktop/Code/Public_Code/weibo_resou.db')
+    # 创建数据库连接，读取数据库
+    engine = create_engine('sqlite:////Users/lawyzheng/Desktop/Code/spider.db')
     df = pandas.read_sql('tb_weibo_resou', con=engine, index_col='index')
-    logger.debug('连接数据库成功。')
+    logger.info('连接数据库成功。')
 
-    #还原still_in数据
+    # 还原still_in数据
     df['still_in'] = False
 
-    #获取时间和时间戳
-    today = datetime.date.today()
-    yesterday = today - datetime.timedelta(days=3)
-
-    today_timestamp = int(time.mktime(today.timetuple()))
-    yesterday_timestamp = int(time.mktime(yesterday.timetuple()))
-
+    # 获取时间和时间戳
     now = int(time.time())
 
-    #获取热搜
-    logger.debug('正在获取热搜榜。')
+    # 获取热搜
+    logger.info('正在获取热搜榜。')
     targets = get_json_data(now)
 
-    #整理数据
-    logger.debug('获取完成，正在进行数据整合。')
+    # 整理数据
+    logger.info('获取完成，正在进行数据整合。')
     df = format_data(df, targets, now)
-    #print(df)
 
-    #获取每分钟的热搜
-    # for timestamp in range(yesterday_timestamp, today_timestamp, 60):
-    #     targets = get_json_data(timestamp)
-    #     df = format_data(df, targets, timestamp)
-
-    #     print(df)
-    #     finished = (timestamp - yesterday_timestamp + 60) / (today_timestamp - yesterday_timestamp)
-    #     print('已完成 %.2f %% 。' % (finished * 100))
-
-    #录入数据库
-    logger.debug('数据整合完成，正在录入数据库。')
+    # 录入数据库
+    logger.info('数据整合完成，正在录入数据库。')
     df.to_sql('tb_weibo_resou', con=engine, index=True, if_exists='replace')
-    logger.debug('录入数据库成功，退出程序。')
+    logger.info('录入数据库成功，退出程序。')
 
 
 if __name__ == '__main__':
